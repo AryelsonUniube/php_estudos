5e3231caf677da8e133ba432cdbaade093908124
AryelsonTevis <tevisaryelson@gmail.com>
2025-04-09 20:27:19 -0300

    style

--- index.php
+++ index.php
@@ -4,19 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/formulario.css">
     <title>Document</title>
 </head>
 
 <body>
+    <div class="body">
 
-    <form action="./src/dados.php" method="post">
-        <label for="">Digite seu cpf:</label>
-        <input type="text" name="cpf" id="cpf"><br>
-        <label for="">Digite sua senha:</label>
-        <input type="text" name="senha" id="senha"><br>
-        <input type="submit" value="Enviar">
-    </form>
+        <div class="formulario">
+            <div class="haeder">LOGIN</div>
+            <form action="./src/dados.php" method="post">
 
+                <div class="cpf">
+                    <input type="text" name="cpf" id="cpf" placeholder="CPF:"><br>
+                </div>
+                <div class="senha">
+                    <input type="password" name="senha" id="senha" placeholder="Senha:"><br>
+                </div>
+
+                <input type="submit" value="Enviar">
+            </form>
+        </div>
+
+    </div>
 </body>
 
 </html>
