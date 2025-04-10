9b1bf87c19c312ef5434312f885a0c58d769043b
Aryelson Tevis <tevisaryelson@gmail.com>
2025-04-10 15:00:11 -0300

    style uptade

--- src/principal.php
+++ src/principal.php
@@ -5,28 +5,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/global.css">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/principal.css">
+
+
 
     <title>Document</title>
 </head>
 
 <body>
     <div class="body">
-        <div class="haeder">
-            <div class="name"><span>Salve <?= $_SESSION['nome']; ?></span></div>
-            <div class="box_button"><span class="button">
-                    <a href="sair.php">sair</a>
-                </span></div>
-        </div>
-        <div class="sidebar">
-            <h2>Menu</h2>
-            <p><a href="item.php">item 1</a></p>
-            <p>item 2 </p>
-            <p>item 3</p>
-        </div>
-        <div class="main">
-            <h2>Manutenção de filmes</h2>
+        <div class="content">
+            <div class="haeder">
+                <div class="name"><span>Salve <?= $_SESSION['nome']; ?></span></div>
+                <div class="box_button"><span class="button">
+                        <a href="sair.php"><img src="../img/1828427.png" alt="Sair"></a>
+                    </span></div>
+            </div>
+            <div class="sidebar">
+                <h2 class="title menu">Menu</h2>
+                <p><a href="item.php">item 1</a></p>
+                <p><a href="item.php">item 2</a> </p>
+                <p><a href="item.php">item 3</a></p>
+            </div>
+            <div class="main">
+                <div class="main content">
+                    <h2 class="title main">Manutenção de filmes</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam debitis optio cupiditate
+                        ipsam
+                        recusandae omnis reprehenderit veniam exercitationem quos possimus perferendis facere
+                        repellendus
+                        soluta quas nobis iste ipsum, tempora quia?
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque reprehenderit, quidem rerum,
+                        corrupti omnis dignissimos veritatis ex quia incidunt tempora cum impedit sequi iste adipisci
+                        facere aliquam illo facilis perspiciatis.</p>
+                </div>
+            </div>
         </div>
     </div>
 </body>
