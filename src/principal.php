eff3910651c7159f6b558916ee882e8e3bd083a8
Aryelson Tevis <tevisaryelson@gmail.com>
2025-04-10 15:56:39 -0300

    Uptade

--- src/principal.php
+++ src/principal.php
@@ -21,23 +21,26 @@
                         <a href="sair.php"><img src="../img/1828427.png" alt="Sair"></a>
                     </span></div>
             </div>
-            <div class="sidebar">
-                <h2 class="title menu">Menu</h2>
-                <p><a href="item.php">item 1</a></p>
-                <p><a href="item.php">item 2</a> </p>
-                <p><a href="item.php">item 3</a></p>
-            </div>
-            <div class="main">
-                <div class="main content">
-                    <h2 class="title main">Manutenção de filmes</h2>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam debitis optio cupiditate
-                        ipsam
-                        recusandae omnis reprehenderit veniam exercitationem quos possimus perferendis facere
-                        repellendus
-                        soluta quas nobis iste ipsum, tempora quia?
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque reprehenderit, quidem rerum,
-                        corrupti omnis dignissimos veritatis ex quia incidunt tempora cum impedit sequi iste adipisci
-                        facere aliquam illo facilis perspiciatis.</p>
+            <div class="main-container">
+                <div class="sidebar">
+                    <h2 class="title menu">Menu</h2>
+                    <p><a href="item.php">Item 1</a></p>
+                    <p><a href="item.php">Item 2</a> </p>
+                    <p><a href="item.php">Item 3</a></p>
+                </div>
+                <div class="main">
+                    <div class="main content">
+                        <h2 class="title main">Manutenção de filmes</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam debitis optio cupiditate
+                            ipsam
+                            recusandae omnis reprehenderit veniam exercitationem quos possimus perferendis facere
+                            repellendus
+                            soluta quas nobis iste ipsum, tempora quia?
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque reprehenderit, quidem rerum,
+                            corrupti omnis dignissimos veritatis ex quia incidunt tempora cum impedit sequi iste
+                            adipisci
+                            facere aliquam illo facilis perspiciatis.</p>
+                    </div>
                 </div>
             </div>
         </div>
